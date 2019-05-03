6fdbaadc6234ed7cdf1d79c21117062973690f30
Savepong <savepong.com@gmail.com>
2019-05-03 11:14:32 +0700

    make user edit

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Http\Request;
 
 Route::namespace('Api')->group(function () {
     Route::get('/users', 'UsersController@index');
+    Route::get('/users/{user}', 'UsersController@show');
+    Route::put('/users/{user}',  'UsersController@update');
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
